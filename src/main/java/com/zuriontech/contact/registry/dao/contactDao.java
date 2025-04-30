c14ccbd1c26b1f5580ac69989cf0b0f19d7852c3
Perpetual Akinyi <you@example.com>
2025-04-30 18:30:52 +0300

    my first commit

---
+++ src/main/java/com/zuriontech/contact/registry/dao/contactDao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zuriontech.contact.registry.dao;
+
+/**
+ *
+ * @author perpetual-akinyi
+ */
+public class contactDao {
+    
+}
---
+++ src/main/java/com/zuriontech/contact/registry/util/DBUtil.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zuriontech.contact.registry.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/contact_db";
+    private static final String USER = "root"; // Change this if your MySQL username is different
+    private static final String PASSWORD = ""; // Put your actual password here
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
