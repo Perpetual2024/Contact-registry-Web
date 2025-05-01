e41d9b8af92ee7be9d2ac99cd2a3a45847ad0486
Perpetual Akinyi <you@example.com>
2025-05-01 13:05:56 +0300

    inserting data into MySQL database

--- src/main/java/com/zuriontech/contact/registry/dao/contactDao.java
+++ src/main/java/com/zuriontech/contact/registry/dao/contactDao.java
@@ -4,6 +4,10 @@
  */
 package com.zuriontech.contact.registry.dao;
 
+import com.zuriontech.contact.registry.util.DBUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 
 /**
@@ -11,8 +15,11 @@ import java.util.Date;
  * @author perpetual-akinyi
  */
 public class contactDao {
-    public void addContact(String FullName, String phoneNumber, String emailAddress, String idNumber, Date dateOfBirth, String gender, String county, String organizationName ){
-        
+    public void addContact(String FullName, String phoneNumber, String emailAddress, String idNumber, Date dateOfBirth, String gender, String county, String organizationName ) throws SQLException{
+        String sql = "INSERT INTO contacts (full_name, phone_number, email_address, id_number, date_of_birth, gender, county, organization_name)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try(Connection conn = DBUtil.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)){}
     }
     
     
