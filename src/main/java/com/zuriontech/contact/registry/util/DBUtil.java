965c64baf3edd1c6c1dc8dc7e75439c7ae1bb7b4
Perpetual Akinyi <you@example.com>
2025-05-01 12:41:29 +0300

    added DBUtil class for MYSQQL connection

--- src/main/java/com/zuriontech/contact/registry/util/DBUtil.java
+++ src/main/java/com/zuriontech/contact/registry/util/DBUtil.java
@@ -9,14 +9,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/contact_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/contact_registry_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root"; // Change this if your MySQL username is different
-    private static final String PASSWORD = ""; // Put your actual password here
+    private static final String PASSWORD = "Perpe2005."; // Put your actual password here
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e){
+            System.err.println("MySQL JBCD Driver not found" + e.getMessage());
+            
+            
         }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
