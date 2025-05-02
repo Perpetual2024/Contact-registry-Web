c81aa1d5a0aeea2670e9028bd681898c0852021c
Perpetual Akinyi <you@example.com>
2025-05-02 16:42:56 +0300

    Just created my contacts servlet

---
+++ src/main/java/com/zuriontech/contact/registry/servlet/ContactsListServlet.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zuriontech.contact.registry.servlet;
+
+import com.zuriontech.contact.registry.dao.ContactDao;
+import com.zuriontech.contact.registry.model.Contacts;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author perpetual-akinyi
+ */
+@WebServlet("/contacts")
+public class ContactsListServlet extends HttpServlet{
+   
+    private final  ContactDao dao = new ContactDao();
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    try {
+    List<Contacts> contactList = dao.getAllContacts();
+    request.setAttribute("contacts", contactList);
+    request.getRequestDispatcher("/contact-list.jsp").forward(request, response);}
+    catch (SQLException e){
+    throw new ServletException("Cannot retrieve Contacts", e);
+    }
+   }
+    
+    
+}
